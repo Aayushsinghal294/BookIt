ff481ca6ab3e206d8ca43b474f95c1fc6a0b36a9
Aayushsinghal294 <23bcs003@iiitdmj.ac.in>
2025-06-15 23:24:52 +0530

    Created Navbar

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg'
+import logo1 from './logo1.png'
 import marvelLogo from './marvelLogo.svg'
 import googlePlay from './googlePlay.svg'
 import appStore from './appStore.svg'
@@ -7,6 +8,7 @@ import profile from './profile.png'
 
 export const assets = {
     logo,
+    logo1,
     marvelLogo,
     googlePlay,
     appStore,
