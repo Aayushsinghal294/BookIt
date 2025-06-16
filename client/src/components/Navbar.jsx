b83f313fc4ec7c3083e8e183033c4a72a84aa5ab
Aayushsinghal294 <23bcs003@iiitdmj.ac.in>
2025-06-16 15:27:26 +0530

    Navbar completed (responsive sidebar)

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,28 +1,31 @@
-import React from 'react'
+import { useState } from 'react'
 import { assets } from '../assets/assets'
 import { Link } from 'react-router-dom'
 import { MenuIcon, SearchIcon, XIcon } from 'lucide-react'
 
 const Navbar = () => {
+
+const [isOpen, setIsOpen] = useState(false)
+
   return (
     <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-center px-6 md:px-16 lg:px-36 py-5'>
       <div className='flex items-center justify-between w-full max-w-6xl'>
         <Link to="/">
           <img src={assets.logo1} alt="" className='w-40 h-auto'/>
         </Link>
-        <div className='max-md:absolute max-md:left-0 max-md:top-0 max-md:font-medium max-md:text-lg z-50 flex flex-col md:flex-row items-center max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 overflow-hidden transition-[width] duration-300'>
-          <XIcon className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer'/>
-          <Link to="/">Home</Link>
-          <Link to="/movies" >Movies</Link>
-          <Link to="/theaters" >Theaters</Link>
-          <Link to="/releases" >Releases</Link>
-          <Link to="/favourite" >Favourites</Link>
+        <div className={`max-md:absolute max-md:left-0 max-md:top-0 max-md:font-medium max-md:text-lg z-50 flex flex-col md:flex-row items-center max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 overflow-hidden transition-[width] duration-300 ${isOpen ? 'max-md:w-full' : 'max-md:w-0'}`}>
+           <XIcon className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer' onClick={() => setIsOpen(!isOpen)}/>
+          <Link onClick={() =>{scrollTo(0,0), setIsOpen(false)}} to="/">Home</Link>
+          <Link onClick={() =>{scrollTo(0,0), setIsOpen(false)}} to="/movies" >Movies</Link>
+          <Link onClick={() =>{scrollTo(0,0), setIsOpen(false)}} to="/theaters" >Theaters</Link>
+          <Link onClick={() =>{scrollTo(0,0), setIsOpen(false)}} to="/releases" >Releases</Link>
+          <Link onClick={() =>{scrollTo(0,0), setIsOpen(false)}} to="/favourite" >Favourites</Link>
         </div>
         <div className='flex items-center gap-4'>
           <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer'/>
           <button className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer' >Login</button>
         </div>
-        <MenuIcon className='max md:ml-4 md:hidden w-8 h-8 cursor-pointer' />
+        <MenuIcon className='max md:ml-4 md:hidden w-8 h-8 cursor-pointer' onClick={()=>setIsOpen(!isOpen)} />
       </div>
     </div>
   )
