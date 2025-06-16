dc28cdcdf736a29d56baf4cbc34c19cf05c0135f
Aayushsinghal294 <23bcs003@iiitdmj.ac.in>
2025-06-16 17:34:02 +0530

    Hero Section completed

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { assets } from '../assets/assets'
-import { CalendarIcon, ClockIcon } from 'lucide-react'
+import { ArrowRight, CalendarIcon, ClockIcon } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
 const HeroSection = () => {
+
+    const navigate = useNavigate()
+
   return (
     <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("assets/backgroundImage.png")] bg-cover bg-center h-screen'>
         <img src={assets.marvelLogo} alt="" className='max-h-11 lg:h-11 mt-20' />
@@ -16,6 +20,11 @@ const HeroSection = () => {
                 <ClockIcon className='w-4.5 h-4.5'/> 2h 8m
              </div>
       </div>
+      <p className='max-w-md text-gray-300'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo odit vitae culpa voluptate perspiciatis ea officia,  laudantium autem in laborum delectus.</p>
+  <button onClick={()=>navigate('/movies')} className='flex items-center gap-1 px-6 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer' >
+    Explore Movies 
+    <ArrowRight className='w-5 h-5' />
+  </button>
     </div>
   )
 }
