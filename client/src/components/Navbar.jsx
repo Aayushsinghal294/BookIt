3d49a0da68a15a9903cd732ad04397d656b922ca
Aayushsinghal294 <23bcs003@iiitdmj.ac.in>
2025-06-17 23:00:34 +0530

    css changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const navigate = useNavigate()
     <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-center px-6 md:px-16 lg:px-36 py-5'>
       <div className='flex items-center justify-between w-full max-w-6xl'>
         <Link to="/">
-          <img src={assets.logo1} alt="" className='w-45 h-auto'/>
+          <img src={assets.logo1} alt="" className='w-44 h-auto'/>
         </Link>
         <div className={`max-md:absolute max-md:left-0 max-md:top-0 max-md:font-medium max-md:text-lg z-50 flex flex-col md:flex-row items-center max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 overflow-hidden transition-[width] duration-300 ${isOpen ? 'max-md:w-full' : 'max-md:w-0'}`}>
            <XIcon className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer' onClick={() => setIsOpen(!isOpen)}/>
